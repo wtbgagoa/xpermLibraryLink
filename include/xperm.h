765dc80c81f4de5b73db0b5a3e4802ed9bd91040
wtbgagoa <wtbgagoa@gmail.com>
2023-08-03 08:04:16 +0200

    Added file xperm.cpp. This is a reorganization by Kasper Peeters of the original xperm.c written by José Martín García.

--- include/xperm.h
+++ include/xperm.h
@@ -46,7 +46,7 @@
  *********************************************************************
  *********************************************************************/
 
-#pragma once
+
 
 #include "wstp.h"
 #include "WolframLibrary.h"
@@ -57,6 +57,97 @@
 #include <math.h>
 #include <string.h>
 #include <time.h>
+
+
+
+/*********************************************************************
+ *                             PROTOTYPES                            *
+ *********************************************************************/
+
+/* Output */
+void print_perm(int *p, int n, int nl);
+void print_array_perm(int *perms, int m, int n, int nl);
+void print_list(int *list, int n, int nl);
+void print_array(int *array, int m, int n, int nl);
+
+/* Lists */
+int equal_list(int *list1, int *list2, int n);
+void copy_list(int *list1, int *list2, int n);
+int position(int i, int *list, int n);
+int position_list(int *matrix, int m, int *row, int n);
+void zeros(int *list, int n);
+void range(int *list, int n);
+void complement(int *all, int al, int *part, int pl, int n,
+		int *com, int *cl);
+void sort(int *list, int *slist, int l);
+void sortB(int *list, int *slist, int l, int *B, int Bl);
+int minim(int *list, int n);
+int maxim(int *list, int n);
+void intersection(int *list1, int l1, int *list2, int l2, int *list,
+		int *l);
+
+/* Permutations */
+int isid(int *list, int n );
+void product(int *p1, int *p2, int *p, int n);
+void inverse(int *p, int *ip, int n);
+int onpoints(int point, int *p, int n);
+void stable_points(int *p, int n, int *list, int *m);
+int first_nonstable_point(int *p, int n);
+void nonstable_points(int *list1, int l1, int *GS, int m, int n,
+	int *list2, int *l2);
+void stabilizer(int *points, int k, int *GS, int m, int n, int *subGS, 
+	int *mm);
+void one_orbit(int point, int *GS, int m, int n, int *orbit, int *ol);
+void all_orbits(int *GS, int m, int n, int *orbits);
+void schreier_vector(int point, int *GS, int m, int n, int *nu, int *w);
+void trace_schreier(int point, int *nu, int *w, int *perm, int n);
+
+/* Algorithms */
+long long int order_of_group(int *base, int bl, int *GS, int m, int n);
+int perm_member(int *p, int *base, int bl, int *GS, int m, int n);
+void schreier_sims_step(int *base, int bl, int *GS, int m, int n, int i,
+	int *T, int mm, int *newbase, int *nbl, int **newGS, int *nm,
+	int *num);
+void schreier_sims(int *base, int bl, int *GS, int m, int n,
+	int *newbase, int *nbl, int **newGS, int *nm, int *num);
+void coset_rep(int *p, int n, int *base, int bl, int *GS, int *m,
+	int *freeps, int fl, int *cr);
+void SGSD(int *vds, int vdsl, int *dummies, int dl, int *mQ,
+          int *vrs, int vrsl, int *repes, int rl, int n,
+          int firstd, int *KD, int *KDl, int *bD, int *bDl);
+void double_coset_rep(int *g, int n, int *base, int bl, int *GS, int m,
+        int *vds, int vdsl, int *dummies, int dl, int *mQ,
+        int *vrs, int vrsl, int *repes, int rl, int *dcr);
+void canonical_perm(int *perm,
+	int SGSQ, int *base, int bl, int *GS, int m, int n,
+	int *freeps, int fl, int *dummyps, int dl, int ob, int metricQ,
+	int *cperm);
+void canonical_perm_ext(int *perm, int n,
+	int SGSQ, int *base, int bl, int *GS, int m,
+	int *frees, int fl,
+        int *vds, int vdsl, int *dummies, int dl, int *mQ,
+        int *vrs, int vrsl, int *repes, int rl,
+	int *cperm);
+
+void stab_chain(int *base, int bl, int *GS, int m, int n, int **chain, int *cl);
+
+void one_orbit_chain(int point, int *GS, int *poslist, int poslistl, int n, int *orbit, int *ol);
+
+void one_schreier_orbit_chain(int point, int *GS, int *poslist, int poslistl,
+	int n, int *orbit, int *ol, int *nu, int *w, int init);
+
+void conjugate_chain(int *base, int bl, int *GS, int m, int n, int *p);
+
+void basechange_chain(int **base, int *bl, int **GS, int *m, int n, int ***chain, int **cl, int *newbase, int newbl);
+
+void appendbasepoint_chain(int **base, int *bl, int ***chain, int **cl, int newbasepoint);
+
+void interchange_chain(int **base, int *bl, int **GS, int *m, int n, int ***chain, int **cl, int j);
+
+void search(int *base, int bl, int *GS, int m, int n, int prop, int *info, int infol,
+        int s, int **GSK, int *mK, int *num);
+
+
 
 /********************************************************************** 
  *                           INTERFACE                                *
@@ -283,118 +374,3 @@ void ML_stabsgs ( int n, int* base, long int bl, int* GS, long int m, int* pts, 
 
 :Evaluate:
 */
-
-
-
-
-/*********************************************************************
- *                             PROTOTYPES                            *
- *********************************************************************/
-
-/* Output */
-void print_perm(int *p, int n, int nl);
-void print_array_perm(int *perms, int m, int n, int nl);
-void print_list(int *list, int n, int nl);
-void print_array(int *array, int m, int n, int nl);
-
-/* Lists */
-int equal_list(int *list1, int *list2, int n);
-void copy_list(int *list1, int *list2, int n);
-int position(int i, int *list, int n);
-int position_list(int *matrix, int m, int *row, int n);
-void zeros(int *list, int n);
-void range(int *list, int n);
-void complement(int *all, int al, int *part, int pl, int n,
-		int *com, int *cl);
-void sort(int *list, int *slist, int l);
-void sortB(int *list, int *slist, int l, int *B, int Bl);
-int minim(int *list, int n);
-int maxim(int *list, int n);
-void intersection(int *list1, int l1, int *list2, int l2, int *list,
-		int *l);
-
-/* Permutations */
-int isid(int *list, int n );
-void product(int *p1, int *p2, int *p, int n);
-void inverse(int *p, int *ip, int n);
-int onpoints(int point, int *p, int n);
-void stable_points(int *p, int n, int *list, int *m);
-int first_nonstable_point(int *p, int n);
-void nonstable_points(int *list1, int l1, int *GS, int m, int n,
-	int *list2, int *l2);
-void stabilizer(int *points, int k, int *GS, int m, int n, int *subGS, 
-	int *mm);
-void one_orbit(int point, int *GS, int m, int n, int *orbit, int *ol);
-void all_orbits(int *GS, int m, int n, int *orbits);
-void schreier_vector(int point, int *GS, int m, int n, int *nu, int *w);
-void trace_schreier(int point, int *nu, int *w, int *perm, int n);
-
-/* Algorithms */
-long long int order_of_group(int *base, int bl, int *GS, int m, int n);
-int perm_member(int *p, int *base, int bl, int *GS, int m, int n);
-void schreier_sims_step(int *base, int bl, int *GS, int m, int n, int i,
-	int *T, int mm, int *newbase, int *nbl, int **newGS, int *nm,
-	int *num);
-void schreier_sims(int *base, int bl, int *GS, int m, int n,
-	int *newbase, int *nbl, int **newGS, int *nm, int *num);
-void coset_rep(int *p, int n, int *base, int bl, int *GS, int *m,
-	int *freeps, int fl, int *cr);
-void SGSD(int *vds, int vdsl, int *dummies, int dl, int *mQ,
-          int *vrs, int vrsl, int *repes, int rl, int n,
-          int firstd, int *KD, int *KDl, int *bD, int *bDl);
-void double_coset_rep(int *g, int n, int *base, int bl, int *GS, int m,
-        int *vds, int vdsl, int *dummies, int dl, int *mQ,
-        int *vrs, int vrsl, int *repes, int rl, int *dcr);
-void canonical_perm(int *perm,
-	int SGSQ, int *base, int bl, int *GS, int m, int n,
-	int *freeps, int fl, int *dummyps, int dl, int ob, int metricQ,
-	int *cperm);
-void canonical_perm_ext(int *perm, int n,
-	int SGSQ, int *base, int bl, int *GS, int m,
-	int *frees, int fl,
-        int *vds, int vdsl, int *dummies, int dl, int *mQ,
-        int *vrs, int vrsl, int *repes, int rl,
-	int *cperm);
-
-void stab_chain(int *base, int bl, int *GS, int m, int n, int **chain, int *cl);
-
-void one_orbit_chain(int point, int *GS, int *poslist, int poslistl, int n, int *orbit, int *ol);
-
-void one_schreier_orbit_chain(int point, int *GS, int *poslist, int poslistl,
-	int n, int *orbit, int *ol, int *nu, int *w, int init);
-
-void conjugate_chain(int *base, int bl, int *GS, int m, int n, int *p);
-
-void basechange_chain(int **base, int *bl, int **GS, int *m, int n, int ***chain, int **cl, int *newbase, int newbl);
-
-void appendbasepoint_chain(int **base, int *bl, int ***chain, int **cl, int newbasepoint);
-
-void interchange_chain(int **base, int *bl, int **GS, int *m, int n, int ***chain, int **cl, int j);
-
-void search(int *base, int bl, int *GS, int m, int n, int prop, int *info, int infol,
-        int s, int **GSK, int *mK, int *num);
-
-
-/* Auxiliary variables & functions */
-
-/* ALPHA */
-	struct alphastruct {
-		/* L */
-		int *L; 	/* We assume that elements of L cannot
-				   be repeated. I only have experimental
-				   evidence for this */
-		int Ll;
-		/* s, d */
-		int *s;
-		int *d;
-		/* other */
-		int *o;				   
-	};
-	
-/* TAB */
-void TAB(int *L, int Ll, int *s1, int *d1, int n, struct alphastruct *ALPHA);
-
-/* Subroutines F1 and F2 */
-void F2(int *TAB1, int *g, int *TAB2, int *sgd, int n);
-
-void F1(int *L, int Ll, int *g, int *list, int *listl, int n, int Deltabl, int *Deltab, int oi, int *DeltaD, struct alphastruct *ALPHA);
